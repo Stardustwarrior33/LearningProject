8592891131c28effa1b14a341ca86a45febb9155
Nils Birkner <n.g.birkner@googlemail.com>
2018-08-12 16:22:52 +0200

    Server

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
-import config, {nodeEnv, logStars } from "./config"
+import http from "http";
+import { logStars } from "./config";
 
-console.log(config, nodeEnv);
-logStars("Function");
+const server = http.createServer();
+
+server.on("request", (req, res) =>{
+    res.write("Hello HTTP!\n");
+    setTimeout(() =>{
+        res.write("I can stream!\n");
+        res.end();
+    }, 3000);
+    let body = [];
+    req.on("data", (chunk) =>{
+        body.push(chunk);
+    }).on("end", () => {
+        body = Buffer.concat(body).toString();
+    }).on("error", (err) =>{
+        logStars(err.stack);
+    });
+});
+
+server.listen(8080);
